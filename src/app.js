098208b106f0eaba810a68579375932e65245d17
Tony Borisov <tony.borisov@accenture.com>
2022-10-20 23:46:07 +0300

    feat: profile view and routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
-const { set } = require('ramda')
+const { set, F } = require('ramda')
 
 const app = express()
 
@@ -10,8 +10,44 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, '/public')))
 
+const accountData = fs.readFileSync(
+	'src/json/accounts.json',
+	'utf8'
+)
+const accounts = JSON.parse(accountData)
+
+const userData = fs.readFileSync('src/json/users.json', 'utf8')
+const users = JSON.parse(userData)
+
 app.get('/', (req, res) => {
-	res.render('index', { title: 'Index' })
+	res.render('index', {
+		title: 'Account Summary',
+		accounts: accounts,
+	})
+})
+
+app.get('/savings', (req, res) => {
+	res.render('account', {
+		account: accounts.savings,
+	})
+})
+
+app.get('/checking', (req, res) => {
+	res.render('account', {
+		account: accounts.checking,
+	})
+})
+
+app.get('/credit', (req, res) => {
+	res.render('account', {
+		account: accounts.credit,
+	})
+})
+
+app.get('/profile', (req, res) => {
+	res.render('profile', {
+		user: users[0],
+	})
 })
 
 app.listen(3000, () => {
